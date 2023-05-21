2c297929d853707713df3bd022f1d9e752fea0fa
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-05-21 15:13:13 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,52 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector('.gallery');
+
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"  
+              data-source="${original}" 
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+gallery.innerHTML = createGalleryItems(galleryItems);
+
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') return;
+
+  const imageSrc = e.target.dataset.source;
+  const imageAlt = e.target.alt;
+
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+      <img src="${imageSrc}" alt="${imageAlt}" class="modal__image">
+    </div>
+  `);
+
+  instance.show();
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+const vimeoPlayer = new Player('vimeo-player');
 
+vimeoPlayer.on('timeupdate', function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+import throttle from 'lodash.throttle';
+
+vimeoPlayer.on('timeupdate', throttle(function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+document.addEventListener('DOMContentLoaded', function() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(currentTime));
+  }
+});
